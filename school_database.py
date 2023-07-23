d073da2d95515a100dca1b879b756f3c0a6c684e
barkhaa-12 <140192304+barkhaa-12@users.noreply.github.com>
2023-07-23 21:55:37 +0530

    Add files via upload
    upload file.

---
+++ school_database.py
@@ -0,0 +1,22 @@
+first_name = input("Enter your first name: ") # String Input.
+last_name = input("Enter your last name: ") # String Input.
+dob = input("Enter your date of birth: ") # int Input.
+standard = input("Enter your standard: ") # int Input.
+age = int(input("Enter your age: ")) # Integer Input.
+address = input("Enter your address: ") # String Input.
+parents_name = input("Enter your parents name: ") # String Input.
+city = input("Enter your city: ") # String Input.
+
+
+
+print("The first name is:",first_name)
+print("The last name is:",last_name)
+print("The dob is:", dob)
+print("The standard is:", standard)
+print("The age is:", age)
+print("Address is:", address)
+print("parents namae is:" , parents_name)
+print("city is :", city)
+      
+
+
